dfe59df42d091071dd440b6e6f487743ddeb488a
JRene4460011 <j.nsengiyum@alustudent.com>
2026-07-18 14:09:15 +0000

    Fix API script output and import order

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
 """Display an employee's TODO list progress using a REST API."""
 
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
 
-    users_url = (
-        "https://jsonplaceholder.typicode.com/users/{}"
-        .format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id
+    )
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id
     )
 
-    todos_url = (
-        "https://jsonplaceholder.typicode.com/todos?userId={}"
-        .format(employee_id)
-    )
-
-    employee = requests.get(users_url).json()
+    employee = requests.get(user_url).json()
     tasks = requests.get(todos_url).json()
 
     completed_tasks = [
-        task for task in tasks if task["completed"] is True
+        task for task in tasks if task.get("completed") is True
     ]
-
-    employee_name = employee["name"]
-    completed_count = len(completed_tasks)
-    total_count = len(tasks)
 
     print(
         "Employee {} is done with tasks({}/{}):".format(
-            employee_name,
-            completed_count,
-            total_count
+            employee.get("name"),
+            len(completed_tasks),
+            len(tasks)
         )
     )
 
     for task in completed_tasks:
-        print("\t {}".format(task["title"]))
+        print("\t {}".format(task.get("title")))
